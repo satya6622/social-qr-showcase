feebf67b8a49042ad0d0d35e1f1807ad369bb994
veesamsatyateja <veesam.satyateja@bijak.in>
2025-09-18 13:25:14 +0530

    New chnages

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,16 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/social-qr-showcase/',
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
+
